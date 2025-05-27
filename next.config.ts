3625bfd1383a2146d4032f9ff97c258f639238d7
nnodim <nnodimobinna@gmail.com>
2025-05-27 03:57:38 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    // update to your API endpoint
+    NEXT_PUBLIC_API_BASE_URL: "http://localhost:3000",
+  },
 };
 
 export default nextConfig;
