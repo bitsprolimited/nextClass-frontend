c0d644427a8c9f832bd199e2e9a10c492592bf85
nnodim <nnodimobinna@gmail.com>
2025-05-29 20:28:22 +0100

    Update styles and colors in globals.css; add new color variables in tailwind.config.ts

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,21 +2,11 @@ import ComingSoon from "@/components/comingSoon";
 import Footer from "@/components/footer";
 import SubscribeNewsletter from "@/components/subscribeNewsletter";
 import Testimonial from "@/components/testimonials";
-import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="flex flex-col  items-center">
       <h1>Home</h1>
-      <Link className="text-blue-500" href="/about-us">
-        About
-      </Link>
-      <Link className="text-blue-500" href="/tutors">
-        Tutors
-      </Link>
-      <Link className="text-blue-500" href="/login">
-        Login
-      </Link>
       <Testimonial />
       <ComingSoon />
       <SubscribeNewsletter />
