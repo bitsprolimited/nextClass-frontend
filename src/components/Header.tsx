69dbcfc4170694ccfa4a8f4dcf9d5c18f21830d5
nnodim <nnodimobinna@gmail.com>
2025-06-01 12:21:23 +0100

    feat: add Header component with navigation menu and login button
    - Implemented Header component to include a logo, navigation menu, and login button.
    - Created NavigationMenu component with necessary subcomponents for navigation.
    - Defined navigation items in constants file for easy management and rendering in the Header.

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList
+} from "@/components/ui/navigation-menu";
+import { navItems } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+
+function Header(): React.JSX.Element {
+  const pathname = usePathname();
+  return (
+    <header className="flex items-center justify-between px-4 py-9">
+      <div className="w-full max-w-4xl mx-auto flex items-center justify-between">
+        <div className="flex items-center">
+          <Image
+            src="/logo.png"
+            alt="Next class Logo"
+            width={100}
+            height={24}
+          />
+        </div>
+        <NavigationMenu className="mx-auto">
+          <NavigationMenuList className="gap-10 flex items-center">
+            {navItems.map((item) => (
+              <NavigationMenuItem key={item.href}>
+                <Link href={item.href} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={`px-0 hover:text-secondary group inline-flex h-9 w-max items-center justify-center text-sm font-medium focus:text-secondary disabled:pointer-events-none disabled:opacity-50 transition-all ${
+                      pathname === item.href ? "text-secondary font-bold" : ""
+                    }`}
+                  >
+                    {item.label}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <Button className="font-medium h-auto px-10 py-3 rounded-full hover:bg-secondary">Login</Button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
