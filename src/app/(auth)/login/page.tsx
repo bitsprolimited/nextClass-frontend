7583c8df2484b536104f7a9fa4f8acaef7f1e66a
nnodim <nnodimobinna@gmail.com>
2026-01-18 21:22:33 +0100

    chore: update better-auth package to version 1.4.10
    feat: log API base URL and better-auth URL in login page
    
    style: adjust layout and sizing in Profile component for better responsiveness
    
    fix: update getLearners API endpoint to /user/learners/me
    
    chore: add .dockerignore to exclude unnecessary files from Docker context
    
    build: create Dockerfile for containerized application setup
    
    docs: add README.Docker.md for building and deploying the application with Docker
    
    chore: add docker-compose configuration for application services

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,6 +5,8 @@ import { Suspense } from "react";
 import Loader from "@/components/Loader";
 
 function Login() {
+  console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
+  console.log(process.env.BETTER_AUTH_URL);
   return (
     <>
       <HeroLogin />
